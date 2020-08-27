5e3d5d0acf7403352241299018d57cbeab327038
dennis.gaspar <dennis.gaspar@sensedia.com>
2020-08-26 21:19:55 -0300

    V2 code version

--- mssc-brewary/src/main/java/guru/springframework/msscbrewery/MsscBreweryApplication.java
+++ mssc-brewary/src/main/java/guru/springframework/msscbrewery/MsscBreweryApplication.java
@@ -1,13 +1,13 @@
-package guru.springframework.msscbrewary;
+package guru.springframework.msscbrewery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MsscBrewaryApplication {
+public class MsscBreweryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MsscBrewaryApplication.class, args);
+		SpringApplication.run(MsscBreweryApplication.class, args);
 	}
 
 }
